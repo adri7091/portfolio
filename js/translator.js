b1b6f3f306f49d940c12ca46c41fbe1039f97f82
adrianqsp <adrianqsp@gmail.com>
2020-10-15 20:26:28 +0200

    adds var to the for index variable

--- js/translator.js
+++ js/translator.js
@@ -57,7 +57,7 @@ function setLanguageByLang(lang) {
         ]   
     };
 
-    for(i in dictionary.translations)
+    for(var i in dictionary.translations)
     {
         if (lang === "enUS")
         {
